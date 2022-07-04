9bb069d466f82ae3b56bbda0bcb9cdd3d376a0fc
Iván Masías <ivan.masias.ortiz@gmail.com>
2022-07-03 21:04:38 -0400

    [ADD] cambio de fecha de movimientos de inventario

---
+++ report_delivery_kit/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'ERP Siglo 21 - Fecha Prevista',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    "description": """ Actualiza la fecha prevista en Stock Piking""",
+    'author': 'ERP Siglo 21',
+    'depends': ['base','sale_stock'],
+    'data': [
+        #'views/account_move_reversal.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'active': False,
+}
